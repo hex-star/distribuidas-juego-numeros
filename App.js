d474b5c2715991bf184bf07129570be18aa5b6b1
Nicolás Estrella <estrella.nico@gmail.com>
2021-09-30 20:13:58 -0300

    inicio

--- App.js
+++ App.js
@@ -1,21 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, Text, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+function Principal({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{fontStyle: 'italic', fontWeight: 'bold', fontSize: 20, margin: 30}}>Pantalla Principal</Text>
+      <Button 
+        title="Ir a pantalla secundaria"
+        onPress={() => {
+          navigation.navigate('Secundaria', {
+            itemId: 86,
+            otherParam: 'Soy el superagente ',
+          });
+        }}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function Secundaria({ route, navigation }) {
+  /* Obtengo los parámetros pasados */
+  const { itemId } = route.params;
+  const { otherParam } = route.params;
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{fontStyle: 'italic', fontWeight: 'bold', fontSize: 20}}>Pantalla Secundaria</Text>
+      <Text style={{margin: 20, fontSize: 20}}>{JSON.stringify(otherParam) + ' ' + JSON.stringify(itemId)}</Text>
+      <View style={{margin:20,justifyContent: 'center'}}>
+        <Button 
+          title="Me llamo otra vez..."
+          onPress={() =>
+            navigation.push('Secundaria', {
+              itemId: Math.floor(Math.random() * 100),
+              otherParam: "y yo soy el agente "
+            })
+          }
+        />
+      </View>
+      <View style={{margin:20,justifyContent: 'center'}}>
+        <Button style={{margin: 20}} title="Principal" onPress={() => navigation.navigate('Principal')} />
+      </View>
+      <View style={{margin:20,justifyContent: 'center'}}>
+        <Button style={{margin: 20}} title="Anterior" onPress={() => navigation.goBack()} />
+      </View>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Principal">
+        <Stack.Screen name="Principal" component={Principal}  
+                      options={{ title: 'Primera' }} />
+        <Stack.Screen name="Secundaria" component={Secundaria} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
