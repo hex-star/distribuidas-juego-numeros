41d7448fb0710cda943e01a3ec805f9a3003bf8c
Motta7000 <agus.motta@hotmail.com>
2021-10-01 21:42:42 -0300

    Falta terminar la pantalla 2 nada mas.
    Falta hacer: Cuando ingresas un numero que aparesca por pantalla el numero ingresado y en que caracteres le pifieste . Que te diga si el caracter esta bien,regular o mal.

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
+
 import { StyleSheet, View, Text, Button, TextInput, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -7,7 +8,7 @@ const styles = StyleSheet.create({
   radioButtonContainer: {
     flexDirection: "row",
     justifyContent: 'space-between',
-    display:'flex',
+    display: 'flex',
     marginLeft: 20
   },
   radioButton: {
@@ -41,13 +42,13 @@ function Principal({ navigation }) {
   const handleChangeRepeat = () => {
     if (!isRepeat) {
       setIsRepeat(true)
-    }  
+    }
   }
 
   const handleChangeNotRepeat = () => {
     if (isRepeat) {
       setIsRepeat(false)
-    }  
+    }
   }
 
   const handleSubmit = () => {
@@ -56,8 +57,8 @@ function Principal({ navigation }) {
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-start' }}>
-      <Text style={{ fontStyle:'normal', fontWeight: 'bold', fontSize: 20, margin: 30 }}>Juego de los números</Text>
-      <TextInput 
+      <Text style={{ fontStyle: 'normal', fontWeight: 'bold', fontSize: 20, margin: 30 }}>Juego de los números</Text>
+      <TextInput
         style={{
           margin: 10,
           textAlign: 'center',
@@ -68,7 +69,7 @@ function Principal({ navigation }) {
           height: 50,
           backgroundColor: '#b4d8b0',
         }}
-        placeholder= 'Ingrese nombre'
+        placeholder='Ingrese nombre'
         onChangeText={text => setNombre(text)}
       />
       <View style={styles.radioButtonContainer}>
@@ -87,26 +88,26 @@ function Principal({ navigation }) {
           <Text style={styles.radioButtonText}>Sin repetición</Text>
         </TouchableOpacity>
       </View>
-      <View style={{marginTop:10, width: 190, height: 190 }}>
-      <Button
-      style={{
-          textAlign: 'center',
-          borderColor: '#060606',
-          borderWidth: 1,
-          borderRadius: 100,
-        }}
-        marginTop="200 px"
-        title="Comenzar"
-        color="#b4d8b0"
-        borderColor="#060606"
-        width="200"
-        onPress={() => {
-          navigation.navigate('Secundaria', {
-            itemId: 86,
-            otherParam: 'Soy el superagente ' + nombre
-          });
-        }}
-      />
+      <View style={{ marginTop: 10, width: 190, height: 190 }}>
+        <Button
+          style={{
+            textAlign: 'center',
+            borderColor: '#060606',
+            borderWidth: 1,
+            borderRadius: 100,
+          }}
+          marginTop="200 px"
+          title="Comenzar"
+          color="#b4d8b0"
+          borderColor="#060606"
+          width="200"
+          onPress={() => {
+            navigation.navigate('Secundaria', {
+              repeat: isRepeat,
+              otherParam: 'Soy el superagente ' + nombre
+            });
+          }}
+        />
       </View>
     </View>
   );
@@ -114,30 +115,133 @@ function Principal({ navigation }) {
 
 function Secundaria({ route, navigation }) {
   /* Obtengo los parámetros pasados */
+  const [numero, setNumero] = useState(null);
+  const [contIngresos, setContIngresos] = useState(0);
+  const [numeroSecreto,setNumeroSecreto] = useState(null);
+  const [fin,setFin] = useState(false);
   const { itemId } = route.params;
   const { otherParam } = route.params;
+  const {repeat} = route.params;
+
+  useEffect( () => {
+    // Actualiza el título del documento usando la API del navegador
+    if ( numeroSecreto == null)
+    {
+     setNumeroSecreto(Math.floor(1000 + Math.random() * 9000));
+    
+
+    }
+    if (repeat == false && numeroSecreto != null )
+    {
+      if (isRepeated(numeroSecreto) == true)
+      {    
+       setNumeroSecreto(Math.floor(1000 + Math.random() * 9000));
+
+        console.log(numeroSecreto)
+      }
+   
+    }
+
+  
+  
+  });
+
+  const isRepeated = (x) => {
+  
+    var cnt = [0,0,0,0,0,0,0,0,0,0]
+    var cont = 4;
+    while (cont > 0){
+      console.log("rem:"+rem)
+      console.log("x:"+x)
+      var rem = x % 10
+      cnt[rem] = cnt[rem] + 1
+      x = x / 10;
+      x=Math.floor(x);
+      cont--;
+    }
+    console.log(cnt)
+
+    for ( var i = 0;i<10;i++){
+      if (cnt[i] > 1)
+      {
+        return true;
+      }
+    }
+    return false;
+
+  }
+
+  const play = () => {
+    
+    if (numero != null && numero.toString().length === 4)
+    {
+      setContIngresos(contIngresos + 1);
+      console.log(contIngresos)
+      
+    }
+    else
+    {
+    
+      alert("El numero ingresado es invalido")
+      console.log(numeroSecreto)
+    }
+
+    if (contIngresos >= 10)
+    {
+      
+      setFin(true);
+    }
+    if (numero.toString() === numeroSecreto.toString())
+    {
+      alert("Ganaste!")
+      setFin(true);
+    }
+
+  }
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text style={{fontStyle: 'italic', fontWeight: 'bold', fontSize: 20}}>Pantalla Secundaria</Text>
-      <Text style={{margin: 20, fontSize: 20}}>{JSON.stringify(otherParam) + ' ' + JSON.stringify(itemId)}</Text>
-      <View style={{margin:20,justifyContent: 'center'}}>
-        <Button 
-          title="Me llamo otra vez..."
+    <View style={{ flex: 1, alignItems: 'center', justifyContent:'space-around' }}>
+      <Text style={{ fontStyle: 'italic', fontWeight: 'bold', fontSize: 20,justifyContent:'flex-start' }}>
+      {fin ? <Text>Nro. Secreto: {numeroSecreto}</Text> : "Nro. Secreto: ????"}
+      </Text>
+      <Text style={{ margin: 20, fontSize: 20 }}></Text>
+
+      <View style={{ margin: 20, justifyContent: 'center',display:'flex',flexDirection:'row',alignItems:'flex-end' }}>
+      <TextInput
+        style={{
+          margin: 10,
+          textAlign: 'center',
+          borderColor: '#060606',
+          borderWidth: 1,
+          borderRadius: 10,
+          width: 200,
+          height: 50,
+          backgroundColor: '#b4d8b0',
+        }}
+        placeholder='Ingrese un numero'
+        onChangeText={text => setNumero(text)}
+      />  
+      <View style={{height:50}}>
+      <Button
+     
+          title="PLAY"
           onPress={() =>
-            navigation.push('Secundaria', {
-              itemId: Math.floor(Math.random() * 100),
-              otherParam: "y yo soy el agente "
-            })
+           play()
           }
         />
       </View>
-      <View style={{margin:20,justifyContent: 'center'}}>
-        <Button style={{margin: 20}} title="Principal" onPress={() => navigation.navigate('Principal')} />
+
       </View>
-      <View style={{margin:20,justifyContent: 'center'}}>
-        <Button style={{margin: 20}} title="Anterior" onPress={() => navigation.goBack()} />
+      {/*
+            <View style={{ margin: 20, justifyContent: 'center' }}>
+        <Button style={{ margin: 20 }} title="Principal" onPress={() => navigation.navigate('Principal')} />
       </View>
-    </View>
+      <View style={{ margin: 20, justifyContent: 'center' }}>
+        <Button style={{ margin: 20 }} title="Anterior" onPress={() => navigation.goBack()} />
+      </View>
+  
+       */}
+       </View>
   );
 }
 
@@ -147,8 +251,8 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Principal">
-        <Stack.Screen name="Principal" component={Principal}  
-                      options={{ title: 'Primera' }} />
+        <Stack.Screen name="Principal" component={Principal}
+          options={{ title: 'Primera' }} />
         <Stack.Screen name="Secundaria" component={Secundaria} />
       </Stack.Navigator>
     </NavigationContainer>
